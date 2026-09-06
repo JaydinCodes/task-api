7722ece53237b8be62cafdc3620317274fd0b604
Jaydin Morrison <4260354@myuwc.ac.za>
2026-09-07 00:46:48 +0200

    added now Hikari for connection pooling

--- src/main/java/com/jaydin/taskapi/service/DatabaseConnectionProvider.java
+++ src/main/java/com/jaydin/taskapi/service/DatabaseConnectionProvider.java
@@ -1,21 +1,34 @@
 package com.jaydin.taskapi.service;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
-public class DatabaseConnectionProvider {
+public class DatabaseConnectionProvider implements AutoCloseable{
 
-    private  String jdbcUrl = "jdbc:h2:mem:testdb";
-    private String username = "sa";
-    private  String password = "ps";
+    private final HikariDataSource dataSource;
     public DatabaseConnectionProvider(String jdbcUrl, String username, String password){
-        this.jdbcUrl = jdbcUrl;
-        this.username = username;
-        this.password = password;
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(30_000);
+        config.setIdleTimeout(600_000);
+
+        this.dataSource = new HikariDataSource(config);
     }
 
     public Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(jdbcUrl, username, password);
+        return dataSource.getConnection();
+    }
+
+    @Override
+    public void close() {
+        dataSource.close();
     }
 
 
