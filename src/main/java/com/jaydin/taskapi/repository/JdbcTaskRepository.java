8b8ac7f578f86a5fab8b45059d3d3e1ba8ef7aea
Jaydin Morrison <4260354@myuwc.ac.za>
2026-09-07 00:15:58 +0200

    JDBC crud finished

--- src/main/java/com/jaydin/taskapi/repository/JdbcTaskRepository.java
+++ src/main/java/com/jaydin/taskapi/repository/JdbcTaskRepository.java
@@ -4,7 +4,6 @@ import com.jaydin.taskapi.model.Task;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class JdbcTaskRepository implements TaskRepository{
@@ -24,6 +23,7 @@ public class JdbcTaskRepository implements TaskRepository{
                 "DESCRIPTION TEXT NOT NULL, " +
                 "COMPLETED BOOLEAN NOT NULL DEFAULT FALSE, " +
                 "CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+
 
         try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
              PreparedStatement createStmt = conn.prepareStatement(createTableSql)) {
@@ -59,16 +59,90 @@ public class JdbcTaskRepository implements TaskRepository{
 
     @Override
     public Task findById(int id) {
-        return null;
+        String fetchSQL = "SELECT * FROM Tasks WHERE ID = ?";
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+            PreparedStatement fetchStmt = conn.prepareStatement(fetchSQL)){
+            fetchStmt.setInt(1, id);
+
+            try(ResultSet rs = fetchStmt.executeQuery()){
+                if (rs.next()){
+                    int taskId = rs.getInt("ID");
+                    String title = rs.getString("TITLE");
+                    String des = rs.getString("DESCRIPTION");
+                    Boolean completed = rs.getBoolean("COMPLETED");
+                    Timestamp time = rs.getTimestamp("CREATED_AT");
+                    Task task = new Task(
+                            taskId, title, des, completed, time
+                    );
+                    return task;
+                }
+            }
+            return null;
+        } catch ( SQLException e) {
+            throw new RuntimeException("Failed to find task", e);
+        }
+
     }
 
     @Override
     public List<Task> findAll() {
-        return Collections.emptyList();
+        String findAllSql = "SELECT * FROM Tasks";
+        List<Task> tasks = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+            PreparedStatement findStmt = conn.prepareStatement(findAllSql)){
+            try(ResultSet rs = findStmt.executeQuery()){
+
+                while (rs.next()){
+                    int taskId = rs.getInt("ID");
+                    String title = rs.getString("TITLE");
+                    String des = rs.getString("DESCRIPTION");
+                    Boolean completed = rs.getBoolean("COMPLETED");
+                    Timestamp time = rs.getTimestamp("CREATED_AT");
+                    Task task = new Task(
+                            taskId, title, des, completed, time
+                    );
+                    tasks.add(task);
+
+                }
+            }
+            return tasks;
+        } catch (SQLException e){
+            throw new RuntimeException("Empty List");
+        }
     }
 
     @Override
     public void deleteById(int id) {
+        String deleteSql = "DELETE FROM Tasks WHERE ID = ?";
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+            PreparedStatement deleteStmt = conn.prepareStatement(deleteSql))   {
+            deleteStmt.setInt(1, id);
+            deleteStmt.executeUpdate();
+        } catch (SQLException e){
+            throw new RuntimeException("Failed to delete Task");
+        }
+    }
+
+    public Task update(Task task) {
+        String updateSql = "UPDATE Tasks SET TITLE = ?, DESCRIPTION = ?, COMPLETED = ? WHERE ID = ?";
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+        PreparedStatement updateStmt = conn.prepareStatement(updateSql)){
+            updateStmt.setString(1, task.getTitle());
+            updateStmt.setString(2, task.getDescription());
+            updateStmt.setBoolean(3, task.isCompleted());
+            updateStmt.setInt(4, task.getId());
+
+            int rowsAffected = updateStmt.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new RuntimeException("No task found with ID: " + task.getId());
+            }
+
+            return task;
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to update task");
+        }
 
     }
 
